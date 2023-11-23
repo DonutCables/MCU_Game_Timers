0e9b124517cf57ba00057256629be34b6c3d765f
DonutCables <donutcatproductions@gmail.com>
2023-11-23 03:06:32 -0500

    added more named colors

--- buckets_networked/lib/led_commands.py
+++ buckets_networked/lib/led_commands.py
@@ -10,8 +10,11 @@ class RGB_Control:
 
     COLOR = {
         "Red": (255, 0, 0),
+        "Orange": (255, 165, 0),
+        "Yellow": (255, 255, 0),
         "Green": (0, 255, 0),
         "Blue": (0, 0, 255),
+        "Purple": (255, 0, 255),
         "Off": (0, 0, 0),
     }
 
